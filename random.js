67060badce2323dec5c610f76c7df09fc8d972c8
vagrant <vagrant@precise32.(none)>
2015-06-02 10:54:45 +0000

    random version 2

--- random.js
+++ random.js
@@ -2,9 +2,9 @@
 
 var numero = Math.random();
 
+var str = ' MAYOR que 0.5 ';
+
 if(mumero <= 0.5){
-	console.log('\n'+numero+ 'es MENOR que 0.5');
+	str = ' MENOR que 0.5 ';
 }
-else{
-	console.log('\n'+numero+ 'es MAYOR que 0.5');
-}
+console.log('\n'+numero+ str);
