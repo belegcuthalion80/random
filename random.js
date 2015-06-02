2e069a73d6321b6af074244dc8c122613f380383
vagrant <vagrant@precise32.(none)>
2015-06-02 10:50:54 +0000

    random version 1

---
+++ random.js
@@ -0,0 +1,10 @@
+//Math.random(); Devuelve un valor entre 0 y 1
+
+var numero = Math.random();
+
+if(mumero <= 0.5){
+	console.log('\n'+numero+ 'es MENOR que 0.5');
+}
+else{
+	console.log('\n'+numero+ 'es MAYOR que 0.5');
+}
